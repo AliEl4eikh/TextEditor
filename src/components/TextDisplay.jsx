54d154e057a894cd95b031fdfb47eabb81e08b7b
AliElsheikh <47045107+AliEl4eikh@users.noreply.github.com>
2023-02-27 14:16:34 +0200

    Add underline and strikethrough functionalities

--- src/components/TextDisplay.jsx
+++ src/components/TextDisplay.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Editor, EditorState, convertToRaw } from "draft-js";
+import { convertToRaw } from "draft-js";
 import "draft-js/dist/Draft.css";
 
 function TextDisplay({ editorState }) {
@@ -8,23 +8,26 @@ function TextDisplay({ editorState }) {
 
   const html = rawContent.blocks.map((block) => {
     let { text, inlineStyleRanges } = block;
+    // sort the array to apply the longer style first so we don't have problems in text
+    inlineStyleRanges.sort(function (a, b) {
+      return b.length - a.length;
+    });
     inlineStyleRanges.forEach((style) => {
       let startIndex = getOffset(text, style.offset);
+      let word = text.substring(startIndex, startIndex + style.length);
+      let styledWord;
       let endIndex = startIndex + style.length;
-      let word = text.substring(startIndex, endIndex);
-      if (style.style == "BOLD") {
-        text =
-          text.slice(0, startIndex) +
-          `<span style="font-weight: bold">${word}</span>` +
-          text.slice(endIndex);
-      } else if (style.style == "ITALIC") {
-        text =
-          text.slice(0, startIndex) +
-          `<span style="font-style: italic">${word}</span>` +
-          text.slice(endIndex);
+      if (style.style === "BOLD") {
+        styledWord = `<b>${word}</b>`;
+      } else if (style.style === "ITALIC") {
+        styledWord = `<i>${word}</i>`;
+      } else if (style.style === "STRIKETHROUGH") {
+        styledWord = `<s>${word}</s>`;
+      } else if (style.style === "UNDERLINE") {
+        styledWord = `<u>${word}</u>`;
       }
+      text = text.slice(0, startIndex) + styledWord + text.slice(endIndex);
     });
-
     return text;
   });
 
@@ -35,15 +38,15 @@ function getOffset(string, index) {
   let isInsideTag = false;
   let j = 0;
   for (let i = 0; i < string.length; i++) {
-    if (string[i - 1] == ">") {
+    if (string[i - 1] === ">") {
       isInsideTag = false;
     }
-    if (string[i] == "<") {
+    if (string[i] === "<") {
       isInsideTag = true;
     }
-    if (isInsideTag == true) {
+    if (isInsideTag === true) {
       continue;
-    } else if (isInsideTag == false && j == index) {
+    } else if (isInsideTag === false && j === index) {
       return i;
     }
     j++;
